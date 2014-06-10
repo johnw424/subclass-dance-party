874442e8784687ddc5dd41687cf5cf73fac854bc
Hack Reactor Students <johnw424@gmail.com>
2014-06-10 16:13:44 -0700

    Fix commit errors

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -33,3 +33,34 @@ describe("blinkyDancer", function() {
     });
   });
 });
+
+describe("popDancer", function() {
+
+  var popDancer;
+  var timeBetweenSteps = 100;
+  var clock;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    popDancer = new PopDancer(10, 20, timeBetweenSteps);
+  });
+
+  it("should have a jQuery $node object", function(){
+    expect(popDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+
+  describe("dance", function(){
+    it("should call step at least once per second", function(){
+      sinon.spy(popDancer, "step");
+      expect(popDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(popDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(popDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
